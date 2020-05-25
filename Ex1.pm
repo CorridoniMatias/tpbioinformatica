e7f14ba32f48a554eff5849d174cf6efe1cf2fd7
Matias Mendez Corridoni <matim97@gmail.com>
2020-05-25 19:28:08 -0300

    Agregada decodificacion de las 6 ORFs de los genes

--- Ex1.pm
+++ Ex1.pm
@@ -2,12 +2,19 @@ use Bio::Seq;
 use Bio::SeqIO;
 use Bio::Factory::EMBOSS;
 
-$input_handler = Bio::SeqIO->new(-file => "$ARGV[0]", -format => 'genbank');
-$gen_mrna_nucleotids = $input_handler->next_seq;
-$gen_mrna_aminoacids = $gen_mrna_nucleotids->translate;
 
-# print $gen_mrna_aminoacids->seq;
-# print "\n";
+#$input_handler = Bio::SeqIO->new(-file => "$ARGV[0]", -format => 'genbank');
+#$nucleotids = $input_handler->next_seq;
+#$aminoacids = $nucleotids->translate();
 
-$output_handler = Bio::SeqIO->new(-file => ">$ARGV[1]", -format => 'fasta');
-$output_handler->write_seq($gen_mrna_nucleotids);
+$factory = new Bio::Factory::EMBOSS; 
+$transeq = $factory->program('transeq'); 
+
+$transeq->run({ -frame => 6,
+                -sequence => $ARGV[0],
+                -outseq  => $ARGV[1]});
+
+#$seqio = Bio::SeqIO->new( -file => $compseqoutfile );
+
+#$output_handler = Bio::SeqIO->new(-file => ">$ARGV[1]", -format => 'fasta');
+#$output_handler->write_seq($aminoacids);
