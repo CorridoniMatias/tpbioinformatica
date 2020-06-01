af122a8e98b50022d6c92ca33a245a197cb7b8d9
pepeciavirella <pep.ciavirella@gmail.com>
2020-06-01 00:34:42 -0300

    Delete commented code and add remote BLAST results

--- Ex1.pm
+++ Ex1.pm
@@ -2,19 +2,9 @@ use Bio::Seq;
 use Bio::SeqIO;
 use Bio::Factory::EMBOSS;
 
-
-#$input_handler = Bio::SeqIO->new(-file => $ARGV[0], -format => 'genbank');
-#$nucleotids = $input_handler->next_seq;
-#$aminoacids = $nucleotids->translate();
-
 $factory = new Bio::Factory::EMBOSS; 
 $transeq = $factory->program('transeq'); 
 
 $transeq->run({ -frame => 6,
                 -sequence => $ARGV[0],
                 -outseq  => $ARGV[1]});
-
-#$seqio = Bio::SeqIO->new( -file => $compseqoutfile );
-
-#$output_handler = Bio::SeqIO->new(-file => ">$ARGV[1]", -format => 'fasta');
-#$output_handler->write_seq($aminoacids);
